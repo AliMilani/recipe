3a2b1b0b9c711c19da30defa7843592903017c20
Ali Milani <59066012+AliMilani@users.noreply.github.com>
2022-09-07 15:44:28 +0430

    Add support for custom response code on server error.

--- src/controllers/middlewares/error.middleware.mjs
+++ src/controllers/middlewares/error.middleware.mjs
@@ -1,11 +1,14 @@
+import mongoose from 'mongoose'
 import { Code } from '../../utils/consts.utils.mjs'
 import { response } from '../../utils/functions.mjs'
 import _ from 'lodash'
 
 export default function (err, req, res, next) {
+    let ErrorCode = err.httpResponseCode ? err.httpResponseCode : Code.SERVER_ERROR
+    if (err instanceof mongoose.Error.ValidationError) ErrorCode = Code.DATABASE_ERROR
     console.error(`${err.message} \n\n ${err.stack || ''}`, err)
     response(res, {
-        code: Code.SERVER_ERROR,
+        code: ErrorCode,
         info: { ..._.pick(err, ['message', 'stack']), err }
     })
     process.exit(1)
