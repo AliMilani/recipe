bfa71905a0b68946bdde33e5a79c3a640ab0aa60
Ali Milani <59066012+AliMilani@users.noreply.github.com>
2022-09-11 16:12:13 +0430

    Update error middleware to have a graceful shutdown.

--- src/controllers/middlewares/error.middleware.mjs
+++ src/controllers/middlewares/error.middleware.mjs
@@ -2,14 +2,23 @@ import mongoose from 'mongoose'
 import { Code } from '../../utils/consts.utils.mjs'
 import { response } from '../../utils/functions.mjs'
 import _ from 'lodash'
+import server from '../../server.mjs'
 
 export default function (err, req, res, next) {
     let ErrorCode = err.httpResponseCode ? err.httpResponseCode : Code.SERVER_ERROR
     if (err instanceof mongoose.Error.ValidationError) ErrorCode = Code.DATABASE_ERROR
-    console.error(`${err.message} \n\n ${err.stack || ''}`, err)
+    if (err instanceof Error) {
+        console.error(`${err.message} \n\n ${err.stack || ''}`)
+    } else if (err) {
+        console.error(err)
+    }
+    res.err = err // to be used in request logger middleware
     response(res, {
         code: ErrorCode,
         info: { ..._.pick(err, ['message', 'stack']), err }
     })
-    process.exit(1)
+
+    // Attempt a graceful shutdown
+    server.close(1)
+    setTimeout(() => process.exit(1), 1000).unref()
 }
